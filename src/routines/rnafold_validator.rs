d2ce6a7731b0bf993dbd261bcc2791ba8c0f5e13
Mika Davies <mika.davies.nt@gmail.com>
2024-11-21 11:03:27 +0900

    Fix validator and add basic similarity statistics

--- src/routines/rnafold_validator.rs
+++ src/routines/rnafold_validator.rs
@@ -1,7 +1,8 @@
 // This does not work yet
 
-use std::{io::Write, process::{Command, ExitStatus}, thread::sleep};
+use std::{io::Write, process::{Command, ExitStatus}};
 
+use indicatif::{ProgressBar, ProgressStyle};
 use strsim::hamming;
 
 use super::{mesh::Mesh, sequencer::{generate_sequence, MotifStorage}};
@@ -12,13 +13,13 @@ pub fn run_fold(sequence: &str) {
     file.write(sequence.as_bytes()).unwrap();
 
     // Wait a bit to ensure contents are written
-    sleep(std::time::Duration::from_millis(10));
+    //sleep(std::time::Duration::from_millis(10));
 
     // Run RNAFold
     log::debug!("Running RNAFold");
     let status: ExitStatus = if cfg!(target_os = "windows") {
         Command::new("cmd")
-            .args(["/C", "rnafold -p -d2 --noLP --circ -i output/sequence.fa > output/sequence.out"])
+            .args(["/C", "rnafold -p -d2 --noLP --circ −−log−level=4 -i output/output.txt > output/sequence.out"])
             .status()
             .expect("failed to execute process")
     } else {
@@ -45,33 +46,102 @@ pub fn fetch_brackets() -> String {
 }
 
 #[inline(always)]
-pub fn compare_brackets(target: &str, similarity_threshold: f64) -> bool {
+pub fn compare_brackets(target: &str) -> f64 {
     let output: String = fetch_brackets();
-    let similarity: f64 = 1.0 - (hamming(target, &output).unwrap() as f64 / 100.0);
-    similarity >= similarity_threshold
+    let similarity: f64 = 100.0 * (1.0 - (hamming(target, &output).unwrap() as f64 / target.len() as f64));
+    //log::info!("Similarity: {similarity:.2}%");
+    similarity
+}
+
+#[inline(always)]
+pub fn similarity_test(
+    runs: usize,
+    mesh: &Mesh,
+    path: &[(bool, usize)],
+    motifs: &MotifStorage
+) -> (f64, ([String;2], f64)) {
+    // Output them
+    let mut mean_similarity: f64 = 0.0;
+    let mut item_count: f64 = 0.0;
+    let mut best_sequence: String = String::new();
+    let mut best_sequence_db: String = String::new();
+    let mut highest_similarity: f64 = 0.0;
+
+    let pb: ProgressBar = ProgressBar::new(runs as u64);
+    pb.set_style(
+        ProgressStyle::with_template("{spinner:.green} [{wide_bar:.cyan/blue}] {pos}/{len} | {msg}").unwrap()
+    );
+
+    (0..runs).for_each(|_| {
+        // Generate sequence
+        let [db_sequence, nt_sequence]: [String; 2] = generate_sequence(mesh, path, motifs);
+        log::debug!("Sequence:\n {nt_sequence}");
+        log::debug!("Brackets:\n {db_sequence}");  
+
+        let mut file = std::fs::File::create("output/output.txt").unwrap();
+        writeln!(file, "{nt_sequence}").unwrap();
+
+        run_fold(&nt_sequence);
+        let similarity: f64 = compare_brackets(&db_sequence);
+
+        if similarity > highest_similarity {
+            highest_similarity = similarity;
+            best_sequence = nt_sequence;
+            best_sequence_db = fetch_brackets();
+        }
+
+        // Average precisions
+        item_count += 1.0;
+        mean_similarity += (similarity - mean_similarity) / item_count;
+
+        log::debug!("Mean similarity: {mean_similarity:.2}%");
+
+        // Update progress bar
+        pb.set_message(format!("Mean similarity: {similarity:.2}%"));
+        pb.set_position(item_count as u64);
+    });
+    pb.finish_with_message("done");
+    (mean_similarity, ([best_sequence, best_sequence_db], highest_similarity))
 }
 
 pub fn run_until_successful(
     mesh: &Mesh, 
     path: &[(bool, usize)], 
     motifs: &MotifStorage,
-    target: &str,
     similarity_threshold: f64,
 ) {
     let mut is_same: bool = false;
     let mut attempt: usize = 1;
+
+    let pb: ProgressBar = ProgressBar::new(20);
+    pb.set_style(
+        ProgressStyle::with_template("{spinner:.green} | {msg}").unwrap()
+    );
+    let mut similarity: f64 = 0.0;
+    let mut final_sequence: String = String::new();
+
     while !is_same {
-        log::info!("Sequence attempt {attempt}");
-        let [_db_sequence, sequence]: [String; 2] = generate_sequence(mesh, path, motifs);
+        //log::info!("Sequence attempt {attempt}");
+        let [db_sequence, sequence]: [String; 2] = generate_sequence(mesh, path, motifs);
         run_fold(&sequence);
-        is_same = compare_brackets(target, similarity_threshold);
+        let local_similarity: f64 = compare_brackets(&db_sequence);
+        is_same = local_similarity >= similarity_threshold;
         attempt += 1;
+        pb.set_message(format!("Attempt {attempt} | Similarity: {local_similarity:.2}%"));
+        if is_same {
+            final_sequence = sequence;
+            similarity = local_similarity;
+        }
     }
+
+    pb.finish_with_message(
+        format!("Viable structure found after {attempt} attempts. Similarity = {similarity:.2}")
+    );
 
     let output: String = fetch_brackets();
 
-    log::info!("Target: \n{target}\n");
-    log::info!("Output: \n{output}\n");
+    log::info!("Sequence: \n{final_sequence}\n");
+    log::info!("Structure: \n{output}\n");
 }
 
 pub fn clean_output() {
